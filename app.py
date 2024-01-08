180c3e7c2294c6914992d69866348cf81bf4d978
rohith4088 <2109486@reva.edu.in>
2024-01-08 12:58:28 +0530

    scaled data pickle file added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pickle
-from Flask import flask,request,app,jsonify,url_for,render_template
+from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy
 import pandas 
 app = Flask(__name__)
@@ -8,3 +8,10 @@ model = pickle.load(open("regressor_model.pkl",'rb'))
 @app.route('/')
 def home():
     return render_template("home.html")
+@app.route('/predict_api',methods = ['POST'])
+def predict_api():
+    data = request.json['data'] #in the form of dictornary
+    print(data)
+    
+    
+ 
