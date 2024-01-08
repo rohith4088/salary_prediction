104fc495c04d8611a0d0e9c6813bb95439d3186e
rohith4088 <2109486@reva.edu.in>
2024-01-08 14:01:31 +0530

    configured post request with postman

--- app.py
+++ app.py
@@ -4,18 +4,21 @@ import numpy as np
 import pandas  as pd
 app = Flask(__name__)
 #load the model
-model = pickle.load(open("regressor_model.pkl",'rb'))
+model = pickle.load(open("reg_model.pkl",'rb'))
+scaling = pickle.load(open("scaling.pkl",'rb'))
 @app.route('/')
 def home():
     return render_template("home.html")
 @app.route('/predict_api',methods = ['POST'])
 def predict_api():
-    data = request.json['data'] #in the form of dictornary
+    data = request.json['data']  # in the form of dictionary
     print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    output = model.predict(data)
-    print(output[0])
-    return jsonify(output[0])
-if __name__ == __main__:
+    print(np.array(list(data.values())).reshape(1, -1))
+    new_data = scaling.transform(np.array(list(data.values())).reshape(1, -1))
+    output = model.predict(new_data)
+    print(output)
+    return jsonify(output.tolist())
+
+if __name__ == "__main__":
     app.run(debug = True)
     
