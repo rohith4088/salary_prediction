f98fa33d1c71540d619152d3eaec9ee8b5c9b97b
rohith4088 <2109486@reva.edu.in>
2024-01-08 13:10:22 +0530

    completed the interface

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pickle
 from flask import Flask,request,app,jsonify,url_for,render_template
-import numpy
-import pandas 
+import numpy as np
+import pandas  as pd
 app = Flask(__name__)
 #load the model
 model = pickle.load(open("regressor_model.pkl",'rb'))
@@ -12,6 +12,10 @@ def home():
 def predict_api():
     data = request.json['data'] #in the form of dictornary
     print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    output = model.predict(data)
+    print(output[0])
+    return jsonify(output[0])
+if __name__ == __main__:
+    app.run(debug = True)
     
-    
- 
